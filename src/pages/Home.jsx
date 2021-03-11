7059e619b080e968a76c1794f934cc6f7bd020bc
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-10 23:21:45 -0600

    Adding navbar and homepage to show

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//import { Col, Row, Container } from "../components/Grid";
+import Navbar from "../components/Navbar/navbar";
+import Homepage from "../components/Homepage/homepage";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <Homepage />
+        </div>
+    );
+};
+
+export default Home;
