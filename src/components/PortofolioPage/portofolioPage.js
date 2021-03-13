49096705392f115b0bdb63bb2d2ad7c068e3eeab
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-13 17:52:22 -0600

    upoading images for portofolio, setting up portofolio json, and trying to call it on portofolio'

--- src/components/PortofolioPage/portofolioPage.js
+++ src/components/PortofolioPage/portofolioPage.js
@@ -1,8 +1,40 @@
 import React from "react";
+import portofolio from "../../portofolio.json";
 import "../../style/style.css";
 
-function PortofolioPage() {
-  return <h2>Portofolio</h2>;
+function PortofolioPage(props) {
+  state = { portofolio };
+
+  removePortofolio = (id) => {
+    const portolio = this.state.portofolio.filter((portofolio) => portofolio.id !== id);
+    this.setState({ portofolio });
+  };
+
+  return (
+    <div>
+      <h2>Portofolio</h2>
+      {this.state.portofolio.map((portolio) => (
+        <div className="card">
+          <div className="img-container">
+            <img alt={props.name} src={props.image} />
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Name:</strong> {props.name}
+              </li>
+              <li>
+                <strong>Occupation:</strong> {props.description}
+              </li>
+              <li>
+                <strong>Location:</strong> {props.github}
+              </li>
+            </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default PortofolioPage;
