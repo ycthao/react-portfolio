0eb07156a069c3b44af6c8903bafd6da20220a0a
Yeem Chij Thao <yc.vangsouathao@hotmail.com>
2021-03-18 17:42:10 -0500

    Trying to change card to portofolio

--- src/components/PortofolioCard/portofolioCard.js
+++ src/components/PortofolioCard/portofolioCard.js
@@ -1,35 +1,57 @@
 import React from "react";
 import "../../style/style.css";
+import { Card } from "react-bootstrap";
 
 function FriendCard(props) {
   return (
-    <div className="card">
-      <div className="img-container">
-        <img alt={props.name} src={props.image} />
-      </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>{props.description}</strong>
-          </li>
-          <li>
-            <strong>{props.technology}:</strong>
-          </li>
-          <li>
-            Github:{" "}
-            <a href={props.github} target="_blank" rel="noreferrer">
-              {props.github}
-            </a>{" "}
-            <br />
-            {/* Deployed: <a href={props.deployed} target="_blank" rel="noreferrer">{props.deployed}</a> */}
-            {props.deployed != null ? props.deployed : "no link"}
-          </li>
-        </ul>
-      </div>
-    </div>
+
+        <Card className="col-3">
+          <Card.Title>
+            <h6>{props.name}</h6>
+          </Card.Title>
+          <Card.Img alt={props.name} src={props.image} />
+          <Card.Body>
+            <Card.Text>
+              <p>{props.description}</p>
+              <p>{props.technology}</p>
+              <p>
+                Github: <a href={props.github}>{props.github}</a>
+                <br />
+                Deployed: <a href={props.deployed}>{props.deployed}</a>
+              </p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+
+
+    // <div className="card">
+    //   <div className="img-container">
+    //     <img alt={props.name} src={props.image} />
+    //   </div>
+    //   <div className="content">
+    //     <ul>
+    //       <li>
+    //         <strong>Name:</strong> {props.name}
+    //       </li>
+    //       <li>
+    //         <strong>{props.description}</strong>
+    //       </li>
+    //       <li>
+    //         <strong>{props.technology}:</strong>
+    //       </li>
+    //       <li>
+    //         Github:{" "}
+    //         <a href={props.github} target="_blank" rel="noreferrer">
+    //           {props.github}
+    //         </a>{" "}
+    //         <br />
+    //         {/* Deployed: <a href={props.deployed} target="_blank" rel="noreferrer">{props.deployed}</a> */}
+    //         {props.deployed != null ? props.deployed : "no link"}
+    //       </li>
+    //     </ul>
+    //   </div>
+    // </div>
   );
 }
 
--- src/components/Wrapper/wrapper.js
+++ src/components/Wrapper/wrapper.js
@@ -2,7 +2,13 @@ import React from "react";
 import "../../style/style.css";
 
 function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+  return (
+    <div className="container" style={{ width: "100%" }}>
+      <div className="row center" >
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
 export default Wrapper;
